10546be8de596b14328dab3f5b7141d0131e99f9
thilinag <thilina.gunasekara@gmail.com>
2021-12-20 21:02:00 +1030

    fix for loop limit

--- day7/part1.js
+++ day7/part1.js
@@ -1,21 +1,21 @@
 // const input = require('./example-input')
 const input = require('./input')
-const data = input.split(',').map(Number);
+const data = input.split(',').map(Number).sort((a,b) => a - b)
 
 let lastFuelCost;
 
-for (i = 0; i < Math.max(...data); i++) {
+for (i = 0; i < data.length; i++) {
     // store the distance to move to aligned 
     const diffs = data.map(hp => (Math.abs(hp - i)))
 
     // 1 distance point takes 1 fuel
     const fuelCost = diffs.reduce((p, c) => p + c, 0)
 
-    // if fuel cost is less than what was recorded, save the new value
+        // if fuel cost is less than what was recorded, save the new value
     if (!lastFuelCost || fuelCost < lastFuelCost) {
         lastFuelCost = fuelCost
     } else { // if fuel cost is increasing, it means we have passed the sweetspot
-        break;
+        break
     }
 }
 
--- day7/part2.js
+++ day7/part2.js
@@ -1,10 +1,10 @@
 // const input = require('./example-input')
 const input = require('./input')
-const data = input.split(',').map(Number);
+const data = input.split(',').map(Number).sort((a,b) => a - b)
 
 let lastFuelCost;
 
-for (i = 0; i < Math.max(...data); i++) {
+for (i = 0; i < data.length; i++) {
     // store the distance to move to aligned 
     const diffs = data.map(hp => (Math.abs(hp - i)))
 
@@ -15,7 +15,7 @@ for (i = 0; i < Math.max(...data); i++) {
     if (!lastFuelCost || fuelCost < lastFuelCost) {
         lastFuelCost = fuelCost
     } else { // if fuel cost is increasing, it means we have passed the sweetspot
-        break;
+        break
     }
 }
 
