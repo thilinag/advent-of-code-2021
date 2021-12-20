63d122a5e37e9ab4663f581d2e21e9f3ac812a69
thilinag <thilina.gunasekara@gmail.com>
2021-12-20 21:00:56 +1030

    use a state instead of array bruteforce for day 6

--- day6/part1.js
+++ day6/part1.js
@@ -1,20 +1,26 @@
-const input = require('./example-input')
-// const input = require('./input')
+// const input = require('./example-input')
+const input = require('./input')
 const data = input.split(',').map(Number);
+const fishStages = 9
 
 const noOfDays = 80;
-let noOfFish = data.length;
+
+// initial state
+const fishSchool = Array.from(Array(fishStages).fill(0))
+
+// hydrate with starting fish school
+data.forEach(fish => fishSchool[fish]++)
 
 for(d = 1; d <= noOfDays; d++) {
-    data.forEach((f, i) => {
-        if (f === 0) {
-            data.push(8)
-            data[i] = 6;
-        } else {
-            data[i] = f - 1;
-        }
-    });
+    // move fish internal timer forward by shifting state
+    const reproducingFish = fishSchool.shift()
+
+    // reset the reproducing fish's inernal timer to 6
+    fishSchool[6] = fishSchool[6] + reproducingFish
+    
+    // add new fish with internal timer of 8
+    fishSchool.push(reproducingFish)
 }
 
-console.log(data.length)
+console.log(fishSchool.reduce((p, c) => p + c, 0))
 // 352195
