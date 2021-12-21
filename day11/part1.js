131e5acca9eb1948b1334a29a5c6e2cc023b8cb0
thilinag <thilina.gunasekara@gmail.com>
2021-12-21 23:41:10 +1030

    add day 11

---
+++ day11/part1.js
@@ -0,0 +1,69 @@
+// const input = require('./example-input')
+const input = require('./input')
+const data = input.split('\n').map(d => d.split('').map(Number));
+
+const flashes = []
+
+function updateEnergy(rowIndex, columnIndex, flashed) {
+    if (
+        rowIndex < 0 || // check left edge
+        columnIndex < 0 || // check top edge
+        rowIndex > data.length - 1 || // check bottom edge
+        columnIndex > data[0].length - 1 // check right edge
+    ) {
+        return
+    }
+
+    const octopus = rowIndex + "_" + columnIndex
+
+    // if we already checked this octopus, stop
+    if (flashed.includes(octopus)) return
+
+    // increase energy of current octopus
+    data[rowIndex][columnIndex]++
+    
+    // if energy is more than 9 we need to flash
+    if (data[rowIndex][columnIndex] > 9) {
+
+        // after flashing it should reset energy level to 0 
+        data[rowIndex][columnIndex] = 0;
+
+        // remember the flashed octopus
+        flashed.push(octopus)
+
+        // update energy for adjacent octopuses
+        // can probably use a loop to update
+        updateEnergy(rowIndex, columnIndex + 1, flashed)
+        updateEnergy(rowIndex, columnIndex - 1, flashed)
+        updateEnergy(rowIndex - 1, columnIndex, flashed)
+        updateEnergy(rowIndex + 1, columnIndex, flashed)
+        updateEnergy(rowIndex - 1, columnIndex - 1, flashed)
+        updateEnergy(rowIndex - 1 , columnIndex + 1, flashed)
+        updateEnergy(rowIndex + 1, columnIndex - 1, flashed)
+        updateEnergy(rowIndex + 1, columnIndex + 1, flashed)
+            
+    }
+}
+
+
+function step() {
+    
+    const flashed = []
+
+    data.forEach((row, i) => {
+        data.forEach((column, j) => {
+            // trigger update enegry of octopuses
+            updateEnergy(i,j, flashed)
+        });
+    });
+
+    // save flashed count for each step
+    flashes.push(flashed.length)
+}
+
+// How many total flashes are there after 100 steps?
+for (let index = 0; index < 100; index++) {
+    step()
+}
+console.table(flashes.reduce((p, c) => p + c, 0))
+// 1679
